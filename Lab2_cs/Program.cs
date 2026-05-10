4ad6c5567f7df1a26fee882318f475e514efa032
tregulaWSB <tomasz.regula1@student.wsb.edu.pl>
2026-05-10 19:41:15 +0200

    Added Playlist class and tested class methods

--- Lab2_cs/Program.cs
+++ Lab2_cs/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Lab2_cs
 {
@@ -42,6 +44,74 @@ namespace Lab2_cs
     }
   }
 
+  public class Playlist
+  {
+    public string Name { get; private set; }
+    private readonly List<Track> tracks = new List<Track>();
+
+    public Playlist(string name)
+    {
+      Name = name;
+    }
+
+    public void AddTrack(Track track)
+    {
+      tracks.Add(track);
+    }
+
+    public void RemoveTrack(Track track)
+    {
+      if (tracks.Contains(track))
+      {
+        tracks.Remove(track);
+      }
+      else
+      {
+        Console.WriteLine($"Brak utworu '{track.Title}' na playliście.");
+      }
+    }
+
+    public string CombineDuration()
+    {
+      double sumDecimal = tracks.Sum(track => track.ComputeDuration());
+      int hours = (int)(sumDecimal / 60);
+      int minutes = (int)(sumDecimal % 60);
+      int seconds = (int)(sumDecimal * 60 % 60);
+      if (hours > 0)
+      {
+        return $"{hours} godz. {minutes} min";
+      }
+      else
+      {
+        return $"{minutes} min {seconds} sek.";
+      }
+    }
+
+    public void DisplayInfo()
+    {
+      Console.WriteLine($"Playlista '{Name}', liczba utworów: {tracks.Count}, całkowita długość: {CombineDuration()}");
+    }
+
+    public void Play()
+    {
+      if (tracks.Count == 0)
+      {
+        Console.WriteLine("Nie można odtworzyć playlisty - brak dodanych utworów.");
+        return;
+      }
+      Console.WriteLine($"Odtwarzanie playlisty '{Name}'");
+      foreach (var track in tracks)
+      {
+        track.Play();
+      }
+    }
+
+    public void Rename(string name)
+    {
+      Name = name;
+    }
+  }
+
   class Program
   {
     static void Main()
@@ -51,10 +121,17 @@ namespace Lab2_cs
       var track3 = new Track("Always Forever", "Cults", "3:44");
       var track4 = new Track("Sultans of Swing", "Dire Straits", "5:48");
 
-      Console.WriteLine(track1.Duration);
-      Console.WriteLine(track1.ComputeDuration());
-      Console.WriteLine(track1.DisplayInfo());
-      track1.Play();
+      var playlist1 = new Playlist("Playlista 1");
+      playlist1.DisplayInfo();
+      
+      playlist1.AddTrack(track1);
+      playlist1.AddTrack(track2);
+      playlist1.AddTrack(track3);
+      playlist1.AddTrack(track4);
+      
+      playlist1.DisplayInfo();
+      
+      playlist1.Play();
     }
   }
 }
